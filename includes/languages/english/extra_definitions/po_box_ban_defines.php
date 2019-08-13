7a937a6ad11fe62e81581c4e19247ffe241806b3
tmccaff <iridude@aol.com>
2019-08-12 21:41:28 -0400

    Add files via upload

---
+++ includes/languages/english/extra_definitions/po_box_ban_defines.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * PO Box Ban
+ * Version 1.3
+ * By Steven300
+ * @copyright Portions Copyright 2004-2008 Zen Cart Team
+ * @copyright Portions Copyright 2003 osCommerce
+ * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+ */
+
+// error for PO Box detection
+define('PO_BOX_ERROR', 'Sorry we do not ship to PO Box addresses, please enter another address');
+
+?>
